511b83fb2ec701d240c82eac8b5acd17abaa7ae2
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2024-03-21 13:03:45 -0400

    Apples to Cookies, Splash Text
    Changed apple sprites to cookie, made trivial adjustments to start and end screen text

--- Assets/Resources/Scripts/EndAnim.cs
+++ Assets/Resources/Scripts/EndAnim.cs
@@ -15,7 +15,7 @@ public class EndAnim : MonoBehaviour {
     void LateUpdate () 
     {
         
-        t.position = new Vector3 (t.position.x + 5,t.position.y,t.position.z); 
+        t.position = new Vector3 (t.position.x + 8,t.position.y,t.position.z); 
         cycles++; 
         if (cycles > 480) t.position = new Vector3(orig.position.x,orig.position.y,orig.position.z);
     }
